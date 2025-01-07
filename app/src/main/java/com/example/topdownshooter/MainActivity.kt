7352490623ad2799e30efc5eb1764d687fd04124
NotInterestedPirralho <a27920@alunos.ipca.pt>
2025-01-07 09:55:43 +0000

    main

--- app/src/main/java/com/example/topdownshooter/MainActivity.kt
+++ app/src/main/java/com/example/topdownshooter/MainActivity.kt
@@ -16,6 +16,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.topdownshooter.login.LoginView
 import com.example.topdownshooter.ui.theme.TopDownShooterTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,17 +28,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TopDownShooterTheme {
+                val navController = rememberNavController()
                 var playerOffset by remember { mutableStateOf(Offset.Zero) }
                 val player = remember { Player(context = this, width = 1080, height = 1920) }
 
-                JoystickView(
+                NavHost(
                     modifier = Modifier.fillMaxSize(),
-                    onMove = { offset ->
-                        playerOffset = offset
-                        player.updateWithJoystick(offset)
+                    navController = navController,
+                    startDestination = Screen.Login.route
+                ) {
+                    composable(Screen.Login.route) {
+                        LoginView(
+                            modifier = Modifier.fillMaxSize(),
+                            onLoginSuccess = {
+                                navController.navigate(Screen.Home.route)
+                            }
+                        )
                     }
-                )
+
+                    composable(Screen.Home.route) {
+                        /*HomeView(
+                            modifier = Modifier.fillMaxSize(),
+                        )*/
+                    }
+                }
             }
         }
     }
+}
+
+sealed class Screen (val route:String){
+    object Login : Screen("login")
+    object Home : Screen("home")
 }
